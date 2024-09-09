af635710b9470122434d4ea0dd68046d3b528c17
Minashehata79 <hejrirhdidjb@gmail.com>
2024-09-09 12:30:11 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('removead').addEventListener('click',function(){
+  
+  document.getElementById('advertAd').style.display="none"
+  
+  document.getElementById('header').style.top="0px"
+  navigator.vibrate(50);
+  
+  document.getElementById('txtfilm').style.marginTop="150px"
+})
